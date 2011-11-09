9d72257edd5e3d14d7da561872aed34b5f6521fd
jaischeema <jaischeema@gmail.com>
2011-11-09 11:19:21 +1100

    Adding the datamapper specific code to the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
-class User < ActiveRecord::Base
-  has_many  :access_grants, :class_name => 'Oauth::AccessGrant'
-  has_secure_password
+class User
+
+# https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rb
+# This class implements the has_secure_password, which is not supported by datamapper.
+# So we need to replicate that functionality here
+# 
+# TODO : replace it with actual code
+# 
+# has_secure_password
+#
+
+  include DataMapper::Resource
+  
+  property :id,                     Serial
+  property :username,               String, :required => true
+  property :email,                  String, :required => true
+  property :password_digest,        String, :required => true
+  property :password_reset_token,   String
+  property :login_token,            String
+  property :active,                 Boolean, :required => true
+  property :admin,                  Boolean, :required => true
+  property :password_reset_sent_at, DateTime
+      
+  has n,  :access_grants, :through => Oauth::AccessGrant
 
   attr_accessible :username, :email, :password, :password_confirmation, :language
   validates_presence_of :password, :on => :create
